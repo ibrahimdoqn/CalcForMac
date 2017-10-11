8f34c43ff04fd87b9937c1fd44b35a8244b266dd
A.T.T <wantto@macbook-air.local>
2017-10-12 01:32:54 +0300

    Initial Commit

---
+++ Hesap/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Hesap
+//
+//  Created by wantto on 12.10.2017.
+//  Copyright © 2017 wantto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
