a2e9853b6c5bd9c46525ddf873002659b1b5c149
İbrahim DOĞAN <33070834+ibrahimdoqn@users.noreply.github.com>
2017-10-25 19:32:31 +0300

    Update main.c

--- Hesap/main.c
+++ Hesap/main.c
@@ -2,7 +2,7 @@
 //  main.c
 //  Hesap
 //  Konuşan hesap makinesi
-//  For Unix Based System
+//  Tested MacOS 10.12
 //  Created by wantto on 12.10.2017.
 //  Copyright © 2017 wantto. All rights reserved.
 //
@@ -23,62 +23,71 @@ int main(int argc, const char * argv[]) {
     if (e==4)goto bol;
     if (e==0){if(spk==1)system("say Güle Güle");exit(0);}
     
-    start:
+start:
     {
-    system("clear");
-    printf("Hesap Makinesi 2.0 By Wantto\nYapmak istediğiniz işlemi Seçiniz\n Topla=1 Çıkar=2 Çarp=3 Böl=4\nSeçiminiz...:");if(spk==1)system("say Yapmak istediğiniz işlemi Seçiniz, Topla bir, Çıkar 2, Çarp 3, Böl 4");scanf("%d",&e);
-    if (e==1)goto topla;
-    if (e==2)goto cikar;
-    if (e==3)goto carp;
-    if (e==4)goto bol;
+        system("clear");
+        printf("Hesap Makinesi 2.0 By Wantto\nYapmak istediğiniz işlemi Seçiniz\n Topla=1 Çıkar=2 Çarp=3 Böl=4\nSeçiminiz...:");if(spk==1)system("say Yapmak istediğiniz işlemi Seçiniz, Topla bir, Çıkar 2, Çarp 3, Böl 4");scanf("%d",&e);
+        if (e==1)goto topla;
+        if (e==2)goto cikar;
+        if (e==3)goto carp;
+        if (e==4)goto bol;
+        goto start;
     }
     
-    topla:
+topla:
     {
-    d=d+1;
+        d=d+1;
         system("clear");printf("Toplama İşlemi\n");if(spk==1)system("say Toplama işlemini seçtiniz");
         printf("Birinci sayıyı giriniz...:");if(spk==1)system("say Birinci Sayıyı giriniz");scanf("%d",&a);
         printf("İkinci sayıyı giriniz...:");if(spk==1)system("say ikinci sayıyı giriniz");scanf("%d",&b);
-    printf("Sonuc...:%d",a+b);
-    printf("\n\n\n%d İşlem yaptınız",d);
+        printf("Sonuc...:%d",a+b);
+        printf("\n\n\n%d İşlem yaptınız",d);
+        a:
         printf("\nÇıkmak için 0\n Aynı İşlemi tekrar etmek için 1\n Farklı bir işlem yapmak için 2 Tulaşyın.\nSeçim...:");if(spk==1)system("say Çıkmak için 0, Aynı İşlemi tekrar etmek için 1, Farklı bir işlem yapmak için 2 Tuşlayın");scanf("%d",&c);system("clear");
         if (c==0){if(spk==1)system("say Güle Güle");exit(0);}if (c==1)goto topla;if (c==2)goto start;
+        goto a;
     }
     
-    cikar:
+cikar:
     {
-    d=d+1;
+        d=d+1;
         system("clear");printf("Çıkarma İşlemi\n");if(spk==1)system("say Çıkarma işlemini şeçtiniz");
         printf("Birinci sayıyı giriniz...:");if(spk==1)system("say Birinci Sayıyı giriniz");scanf("%d",&a);
         printf("İkinci sayıyı giriniz...:");if(spk==1)system("say ikinci sayıyı giriniz");scanf("%d",&b);
-    printf("Sonuç%d",a-b);
-    printf("\n\n\n%d İşlem yaptınız",d);
+        printf("Sonuç%d",a-b);
+        printf("\n\n\n%d İşlem yaptınız",d);
+        b:
         printf("\nÇıkmak için 0\n Aynı İşlemi tekrar etmek için 1\n Farklı bir işlem yapmak için 2 Tulaşyın.\nSeçim...:");if(spk==1)system("say Çıkmak için 0, Aynı İşlemi tekrar etmek için 1, Farklı bir işlem yapmak için 2 Tuşlayın");scanf("%d",&c);system("clear");
         if (c==0){if(spk==1)system("say Güle Güle");exit(0);}if (c==1)goto cikar;if (c==2)goto start;
+        goto b;
     }
     
-    carp:
+carp:
     {
-    d=d+1;
+        d=d+1;
         system("clear");printf("Çarpma İşlemi\n");if(spk==1)system("say Çarpma  işlemini seçtiniz");
         printf("Birinci sayıyı giriniz...:");if(spk==1)system("say Birinci Sayıyı giriniz");scanf("%d",&a);
         printf("İkinci sayıyı giriniz...:");if(spk==1)system("say ikinci sayıyı giriniz");scanf("%d",&b);
-    printf("TSonuç%d",a*b);
-    printf("\n\n\n%d İşlem yaptınız",d);
+        printf("TSonuç%d",a*b);
+        printf("\n\n\n%d İşlem yaptınız",d);
+        c:
         printf("\nÇıkmak için 0\n Aynı İşlemi tekrar etmek için 1\n Farklı bir işlem yapmak için 2 Tulaşyın.\nSeçim...:");if(spk==1)system("say Çıkmak için 0, Aynı İşlemi tekrar etmek için 1, Farklı bir işlem yapmak için 2 Tuşlayın");scanf("%d",&c);system("clear");
         if (c==0){if(spk==1)system("say Güle Güle");exit(0);}if (c==1)goto carp;if (c==2)goto start;
+        goto c;
     }
     
-    bol:
+bol:
     {
-    d=d+1;
+        d=d+1;
         system("clear");printf("Bölme işlemi\n");if(spk==1)system("say Bölme işlemini seçtiniz");
         printf("Birinci sayıyı giriniz...:");if(spk==1)system("say Birinci Sayıyı giriniz");scanf("%d",&a);
         printf("İkinci sayıyı giriniz...:");if(spk==1)system("say ikinci sayıyı giriniz");scanf("%d",&b);
-    printf("Sonuç%d",a/b);
-    printf("\n\n\n%d İşlem yaptınız",d);
-        printf("\nÇıkmak için 0\n Aynı İşlemi tekrar etmek için 1\n Farklı bir işlem yapmak için 2 Tulaşyın.\nSeçim...:");if(spk==1)system("say Çıkmak için 0, Aynı İşlemi tekrar etmek için 1, Farklı bir işlem yapmak için 2 Tuşlayın");scanf("%d",&c);system("clear");
+        printf("Sonuç%d",a/b);
+        printf("\n\n\n%d İşlem yaptınız",d);
+        d:
+        printf("\nÇıkmak için 0\n Aynı İşlemi tekrar etmek için 1\n Farklı bir işlem yapmak için 2 Tulaşyın.\nSeçim...:");if(spk==1)system("say Çıkmak için 0, Aynı İşlemi tekrar etmek için 1, Farklı bir işlem yapmak için 2 Tuşlayın");c=getchar();scanf("%d",&c);system("clear");
         if (c==0){if(spk==1)system("say Güle Güle");exit(0);}if (c==1)goto bol;if (c==2)goto start;
+        goto d;
     }
     
     return 0;
